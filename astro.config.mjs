e9cad3db87e5af1db93d4ad8839402a13503e0c0
dnlnm <danielnaim5543@gmail.com>
2026-02-13 05:20:48 +0800

    feat: Implement Cloudflare Workers deployment, integrate starlight-blog, and add NexaPad documentation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,32 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide';
 import starlightImageZoom from 'starlight-image-zoom';
+import starlightBlog from 'starlight-blog';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightThemeRapide(), starlightImageZoom()],
-			title: 'NximKB',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com' }],
-			sidebar: [
-				{
-					label: 'NexaPad',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Information', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          plugins: [starlightThemeRapide(), starlightImageZoom(), starlightBlog()],
+          title: 'NximKB',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com' }],
+          sidebar: [
+              {
+                  label: 'NexaPad',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Information', slug: 'nexapad/information' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
