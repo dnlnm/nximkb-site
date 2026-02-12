8eb1a0ef6dfe3298e37b92cc2f7b0903600e595a
dnlnm <danielnaim5543@gmail.com>
2026-02-13 04:21:55 +0800

    nximkb-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
+import starlightImageZoom from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [starlightThemeRapide(), starlightImageZoom()],
+			title: 'NximKB',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'NexaPad',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Information', slug: 'guides/example' },
 					],
 				},
 				{
